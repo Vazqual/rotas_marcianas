814113bce4d0e9a5d910692a9417a9cf1d1a42dc
Vazqual <cc21233@g.unicamp.br>
2022-11-30 18:57:23 -0300

    Leitura de arquivos JSON e armazenamento dos dados nos Dropdowns para Origem e Destino

--- app/src/main/java/br/unicamp/rotas_marcianas/MainActivity.java
+++ app/src/main/java/br/unicamp/rotas_marcianas/MainActivity.java
@@ -3,30 +3,36 @@ package br.unicamp.rotas_marcianas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 
+
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Cidade[] cidades;
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -35,36 +41,103 @@ public class MainActivity extends AppCompatActivity {
         Gson gson = new Gson();
         Type listUserType = new TypeToken<List<Cidade>>() { }.getType();
 
-        List<Cidade> cidades = gson.fromJson(jsonFileString, listUserType);
-        for (int i = 0; i < cidades.size(); i++) {
-            Log.i("data", "> Item " + i + "\n" + cidades.get(i));
+        List<Cidade> listaCidades = gson.fromJson(jsonFileString, listUserType);
+
+
+
+        cidades = new Cidade[listaCidades.size()];
+        for (int i = 0; i< listaCidades.size(); i++)
+        {
+            cidades[i] = listaCidades.get(i);
+            Log.i("item", cidades[i].toString());
         }
 
+        String[] items = new String[listaCidades.size()];
 
-        Spinner ddOrigem = findViewById(R.id.spnOrigem);
-        Spinner ddDestino = findViewById(R.id.spnDestino);
-        String[] items = new String[cidades.size()];
-        for (int i = 0; i< cidades.size(); i++)
-            items[i] = cidades.get(i).nome;
+        for (int i = 0; i< listaCidades.size(); i++)
+            items[i] = cidades[i].nome;
 
         ArrayAdapter<String> adapterDdOrigem = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         ArrayAdapter<String> adapterDdDestino = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        ddOrigem.setAdapter(adapterDdOrigem);
-        ddDestino.setAdapter(adapterDdDestino);
-        ddOrigem.setSelection(0);
-        ddDestino.setSelection(0);
-        ddOrigem.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view,
-                                       int position, long id) {
-                Log.v("item", (String) parent.getItemAtPosition(position));
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                // TODO Auto-generated method stub
-            }
-        });
+        Spinner ddOrigem = findViewById(R.id.spnOrigem);
+        Spinner ddDestino = findViewById(R.id.spnDestino);
+
+        if (ddOrigem != null && ddDestino != null)
+        {
+            ddOrigem.setAdapter(adapterDdOrigem);
+            ddDestino.setAdapter(adapterDdDestino);
+
+            ddOrigem.setSelection(0);
+            ddDestino.setSelection(0);
+
+            ddOrigem.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view,
+                                           int position, long id) {
+                    Log.v("item", (String) parent.getItemAtPosition(position));
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    // TODO Auto-generated method stub
+                }
+            });
+            ddDestino.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view,
+                                           int position, long id) {
+                    Log.v("item", (String) parent.getItemAtPosition(position));
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    // TODO Auto-generated method stub
+                }
+            });
+        }
+
+        BitmapFactory.Options myOptions = new BitmapFactory.Options();
+        myOptions.inDither = true;
+        myOptions.inScaled = false;
+        myOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;// important
+        myOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.mapa, myOptions);
+        Paint paint = new Paint();
+        paint.setTextSize(30f);
+        paint.setAntiAlias(true);
+        paint.setColor(Color.BLUE);
+
+        Bitmap workingBitmap = Bitmap.createBitmap(bitmap);
+        Bitmap mutableBitmap = workingBitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+        Canvas canvas = new Canvas(mutableBitmap);
+
+        desenharCidades(workingBitmap.getWidth(), workingBitmap.getHeight(), paint, canvas, cidades);
+
+
+
+        ImageView imageView = findViewById(R.id.m);
+        imageView.setAdjustViewBounds(true);
+        imageView.setImageBitmap(mutableBitmap);
+    }
+
+    private void desenharCidades(int largura, int altura, Paint paint, Canvas canvas, Cidade[] cidade) {
+        for (int i = 0; i < cidade.length; i++)
+        {
+            canvas.drawCircle(          //Desenha os pontos nas coordenadas
+                    (float) Math.round(largura*cidade[i].x),    //das cidades
+                    (float) Math.round(altura*cidade[i].y),
+                    10f,
+                    paint);
+            canvas.drawText(            //Escreve os nomes das cidades
+                    cidade[i].nome,     //nos locais apropriados
+                    (float) Math.round(largura*cidade[i].x) + 10f,
+                    (float) Math.round(altura*cidade[i].y),
+                    paint);
+        }
+
     }
 
 
